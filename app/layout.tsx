b07eb71dceb81465e7cd5b0ce09dfa3e85dd1c0a
Dayana Keo <keo.dayana@hotmail.fr>
2025-08-21 15:57:38 +0200

    Ajoute l'écouteur ReminderListener et les notifications temps réel dans le layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,21 @@
 import Providers from '@/components/Providers'
 import Header from '@/components/Header'
 import './globals.css'
+import RealtimeNotifications from '@/components/RealtimeNotifications';
+import { authOptions } from '@/lib/authOptions';
+import { getServerSession } from 'next-auth';
+import ReminderListener from '@/components/ReminderListener';
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+    const session = await getServerSession(authOptions);
     return (
         <html lang="fr">
         <body>
         <Providers>
             <Header />
             {children}
+            {session?.user?.id && <RealtimeNotifications userId={Number(session.user.id)} />}
+             <ReminderListener />
         </Providers>
         </body>
         </html>
